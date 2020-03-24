68b0715a9f64397833787e7883b9581b2cfa32d9
JosafatV <josav09@gmail.com>
2020-03-24 15:42:15 -0600

    Added python parser. TODO cross compile and test on rasp

--- server/client/py_socket.py
+++ server/client/py_socket.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import time
+import json
 
 HOST = "localhost"
 PORT = 8080
@@ -14,4 +15,13 @@ while (True):
     s.send(msg)
     data = s.recv(len(msg)+40000)
     s.close()
-    print ("Received: ", data.decode())
+    server_response = data.decode()
+    
+    parsed_json = json.loads(server_response)
+
+    print("Led 1: " + str(parsed_json['led_1']))
+    print("Led 2: " + str(parsed_json['led_2']))
+    print("Led 3: " + str(parsed_json['led_3']))
+    print("Led 4: " + str(parsed_json['led_4']))
+    print("Led 5: " + str(parsed_json['led_5']))
+
