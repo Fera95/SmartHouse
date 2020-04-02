19077ea6554e1a0c63f6e3e1dfe6f0e6e8157506
Fera95 <ferarios95@gmail.com>
2020-04-01 18:32:32 -0600

    Se agregan displays en register
    Se agregan mesajes de exito y error en el registro de usuarios

--- smarthouse/src/app/register/register.page.ts
+++ smarthouse/src/app/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../services/auth.service'; 
 import {User} from '../shared/user.class'; 
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -10,7 +11,7 @@ import {User} from '../shared/user.class';
 export class RegisterPage implements OnInit {
   user: User = new User();
 
-  constructor(private authSvc: AuthService,private router: Router) { }
+  constructor(private authSvc: AuthService,private router: Router,private toastCtrl: ToastController) { }
 
   ngOnInit() {
   }
@@ -18,9 +19,20 @@ export class RegisterPage implements OnInit {
   async onRegister(){
     const user = await this.authSvc.OnRegister(this.user);
     if(user){
-      console.log('Usario creado con exito!');
+      this.showToast('Usario creado con exito!');
       this.router.navigateByUrl('/');
     }
+    else{
+      this.showToast('Error creando usuario,recuerde utilizar formato válido de correo y que la contraseña debe ser de almenos 6 carácteres.');
+    }
+  }
+
+
+  showToast(msg) {
+    this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    }).then(toast => toast.present());
   }
 
 }
